eefd70ee1164f4dd771c82f1f7ad319e69e65d47
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-09-12 01:11:23 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,30 +1,69 @@
-from localgraphclustering import *
-
-import time
+# import sys
+# sys.path.append("../LocalGraphClustering/")
+import localgraphclustering as lgc
+from collections import Counter,defaultdict
 import numpy as np
 
-# Read graph. This also supports gml and graphml format.
-g = GraphLocal('notebooks/datasets/senate.edgelist','edgelist',' ')
+rptr = open("notebooks/dataset/cora/cora.content","r")
+lines = rptr.readlines()
+rptr.close()
 
-# Call the global spectral partitioning algorithm.
-eig2 = fiedler(g)
+ids = {}
+labels = {}
+clusters = defaultdict(list)
+nnodes = 0
+for i,line in enumerate(lines):
+    tmp = line.replace("\n","").split("\t")
+    labels[i] = tmp[-1]
+    ids[tmp[0]] = i
+    nnodes += 1
+    clusters[tmp[-1]].append(i)
 
-# Round the eigenvector
-output_sc = sweep_cut(g,eig2)
+rptr = open("dataset/cora/cora.cites","r")
+lines = rptr.readlines()
+rptr.close()
 
-# Extract the partition for g and store it.
-eig2_rounded = output_sc[0]
+ei,ej = [],[]
+for line in lines:
+    tmp = line.replace("\n","").split("\t")
+    if tmp[0] not in ids:
+        ids[tmp[0]] = nnodes
+        nnodes += 1
+    if tmp[1] not in ids:
+        ids[tmp[1]] = nnodes
+        nnodes += 1
+    ei.append(ids[tmp[0]])
+    ej.append(ids[tmp[1]])
 
-# Conductance before improvement
-print("Conductance before improvement:",g.compute_conductance(eig2_rounded))
+g = lgc.GraphLocal()
+g.list_to_gl(ei,ej,[1.0]*len(ei))
+g.connected_components()
+largest_component= set(np.nonzero(g.components == Counter(g.components).most_common()[0][0])[0])
+g_copy = g.largest_component()
+clusters_copy = defaultdict(list)
+nnodes = 0
+for i in range(g._num_vertices):
+    if i in largest_component:
+        if i in labels:
+            clusters_copy[labels[i]].append(nnodes)
+        nnodes += 1
 
-# Start calling SimpleLocal
-start = time.time()
-output_SL_fast = SimpleLocal(g,eig2_rounded)
-end = time.time()
-print("running time:",str(end-start)+"s")
+kwargs_list = [{"method":"l1reg-rand",
+                "alpha":0.1,
+                "rho":1.0e-10,
+                "normalize":False,
+                "normalized_objective":False,
+                "epsilon":1.0e-8,
+                "iterations":100000,
+                "cpp":True,
+                "delta":0.01,
+                "ratio":ratio} for ratio in [5, 10, 20]] #0.01*np.array(range(5,26))]
 
-# Conductance after improvement
-print("Conductance after improvement:",g.compute_conductance(output_SL_fast[0]))
+records = semisupervised_learning(g_copy,clusters_copy,kwargs_list,nprocs=40,flowmethod="sl")
 
-output_SL = output_SL_fast[0]
+
+x = [5, 10, 20] # 0.01*np.array(range(5,26))
+y1 = np.array([i[0] for i in records["l1reg_F1_all"]])
+
+print(x)
+print(y1)
